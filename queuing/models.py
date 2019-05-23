836a8b4a6974eb46df7487d21f957317ceafe127
Lepiku <git.lepiku@gmail.com>
2019-05-23 16:17:21 +0700

    migrate and edit models.phone

--- queuing/models.py
+++ queuing/models.py
@@ -4,9 +4,9 @@ from django.core.validators import RegexValidator
 
 class Phone(models.Model):
     phone_regex = RegexValidator(regex=r'^(\+62|0)\d{9,15}$', \
-            message="Phone number must be entered in the format: '+999999999'. \
-                    Up to 15 digits allowed.")
-    phone_number = models.CharField(validators=[phone_regex], max_length=17, \
+            message="Phone number must be entered in the format: \
+                    '+628...' or '08...'. Up to 15 digits allowed.")
+    phone_number = models.CharField(validators=[phone_regex], max_length=18, \
             blank=True)
 
 class Pengguna(models.Model):
@@ -17,14 +17,6 @@ class Pengguna(models.Model):
     def __str__(self):
         return str(self.nama)
 
-class Pendaftaran(models.Model):
-    watu_daftar = models.DateTimeField(auto_now=true)
-    pengguna = models.ForeignKey(Pengguna, on_delete=models.CASCADE)
-    hari = models.ForeignKey(Hari, on_delete=models.CASCADE)
-
-    def __str__(self):
-        return str(self.pengguna) + ": " + str(self.hari)
-
 class Hari(models.Model):
     tanggal = models.DateField()
     waktu_buka = models.TimeField()
@@ -32,3 +24,12 @@ class Hari(models.Model):
 
     def __str__(self):
         return str(self.tanggal)
+
+class Pendaftaran(models.Model):
+    watu_daftar = models.DateTimeField(auto_now=True)
+    pengguna = models.ForeignKey(Pengguna, on_delete=models.CASCADE)
+    hari = models.ForeignKey(Hari, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return str(self.pengguna) + ": " + str(self.hari)
+
