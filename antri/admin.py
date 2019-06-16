70278299ccca292afe2cf20cb0593f6526259c69
Lepiku <git.lepiku@gmail.com>
2019-06-16 14:33:45 +0700

    update admin page and models (Pendaftar)

--- antri/admin.py
+++ antri/admin.py
@@ -1,8 +1,19 @@
 from django.contrib import admin
-from antri import models
+from antri.models import Pengguna, KepalaKeluarga, Pendaftaran, Hari, Pendaftar
+
+class PenggunaAdmin(admin.ModelAdmin):
+    list_display = ('__str__', 'kepala_keluarga', 'user')
+
+class PendaftarInline(admin.TabularInline):
+    model = Pendaftar
+    extra = 1
+
+class PendaftaranAdmin(admin.ModelAdmin):
+    list_display = ('kepala_keluarga', 'hari')
+    inlines = [PendaftarInline]
 
 # Register your models here.
-admin.site.register([
-    models.Pengguna, models.KepalaKeluarga, models.Pendaftaran, models.Hari
-    ])
+admin.site.register([KepalaKeluarga, Hari, Pendaftar])
+admin.site.register(Pengguna, PenggunaAdmin)
+admin.site.register(Pendaftaran, PendaftaranAdmin)
 
--- antri/models.py
+++ antri/models.py
@@ -6,22 +6,27 @@ REGEX_TELP = RegexValidator(regex=r'^(\+62|0)\d{9,15}$', \
         message="Nomor Handphone harus mengikuti format: \
                 '+628...' or '08...' maksimal 15 digit.")
 JENIS_KELAMIN = (('l', 'Laki-Laki'), ('p', 'Perempuan'))
+NAME_LENGTH = 64
 
 class KepalaKeluarga(models.Model):
-    nama = models.CharField(max_length=128)
+    nama = models.CharField(max_length=NAME_LENGTH)
     alamat = models.TextField()
+    waktu_daftar = models.DateTimeField(auto_now_add=True)
+    waktu_ubah = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return "{} ({})".format(self.nama, self.pengguna_set.first())
 
 class Pengguna(models.Model):
-    nama = models.CharField(max_length=128)
+    nama = models.CharField(max_length=NAME_LENGTH)
     tanggal_lahir= models.DateField()
     jenis_kelamin = models.CharField(max_length=1, choices=JENIS_KELAMIN)
     telp = models.CharField(validators=[REGEX_TELP], max_length=18)
     kepala_keluarga = models.ForeignKey(KepalaKeluarga,
             on_delete=models.CASCADE)
     user = models.OneToOneField(User, on_delete=models.CASCADE)
+    waktu_daftar = models.DateTimeField(auto_now_add=True)
+    waktu_ubah = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         if len(self.nama) > 32:
@@ -37,10 +42,17 @@ class Hari(models.Model):
         return '{} ({})'.format(self.tanggal, len(self.pendaftaran_set.all()))
 
 class Pendaftaran(models.Model):
-    watu_daftar = models.DateTimeField(auto_now=True)
-    pengguna = models.ForeignKey(Pengguna, on_delete=models.CASCADE)
+    watu_daftar = models.DateTimeField(auto_now_add=True)
+    kepala_keluarga = models.ForeignKey(KepalaKeluarga,
+            on_delete=models.CASCADE)
     hari = models.ForeignKey(Hari, on_delete=models.CASCADE)
 
     def __str__(self):
-        return str(self.pengguna) + ": " + str(self.hari)
+        return str(self.pengguna) + ": " + str(self.hari.tanggal)
 
+class Pendaftar(models.Model):
+    pendaftaran = models.ForeignKey(Pendaftaran, on_delete=models.CASCADE)
+    nama = models.CharField(max_length=NAME_LENGTH)
+
+    def __str__(self):
+        return str(self.nama)
