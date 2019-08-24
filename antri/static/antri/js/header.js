dd708c9471f41147a47b4e794519960b1cf921e7
Lepiku <git.lepiku@gmail.com>
2019-08-24 18:34:22 +0700

    jaiden animations

--- antri/static/antri/js/header.js
+++ antri/static/antri/js/header.js
@@ -1,5 +1,6 @@
 var menu = document.getElementById('menu-content')
 var menu_cover = document.getElementById('menu-cover')
+var bar = document.getElementById('bar')
 
 function show_menu() {
   if (menu.classList.contains('show')) {
@@ -7,12 +8,14 @@ function show_menu() {
   } else {
     menu.classList.add('show')
     menu_cover.classList.add('show-cover')
+    bar.classList.add('close')
   }
 }
 function hide_menu() {
   if (menu.classList.contains('show')) {
     menu.classList.remove('show')
     menu_cover.classList.remove('show-cover')
+    bar.classList.remove('close')
   }
 }
 document.getElementById('container').addEventListener('click', hide_menu);
