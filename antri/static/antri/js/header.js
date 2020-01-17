814a775b6a8fcb5af49033570596e1be6c17dc04
Lepiku <git.lepiku@gmail.com>
2020-01-17 10:49:19 +0700

    dropdown behind navbar

--- antri/static/antri/js/header.js
+++ antri/static/antri/js/header.js
@@ -1,21 +1,21 @@
-var menu = document.getElementById('menu-content')
-var menu_cover = document.getElementById('menu-cover')
-var bar = document.getElementById('bar')
+var menu = document.getElementById("menu-content");
+var menu_cover = document.getElementById("menu-cover");
+var bar = document.getElementById("bar");
 
 function show_menu() {
-  if (menu.classList.contains('show')) {
-    hide_menu()
+  if (menu.classList.contains("show")) {
+    hide_menu();
   } else {
-    menu.classList.add('show')
-    menu_cover.classList.add('show-cover')
-    bar.classList.add('close')
+    menu.classList.add("show");
+    menu_cover.classList.add("show-cover");
+    bar.classList.add("close");
   }
 }
 function hide_menu() {
-  if (menu.classList.contains('show')) {
-    menu.classList.remove('show')
-    menu_cover.classList.remove('show-cover')
-    bar.classList.remove('close')
+  if (menu.classList.contains("show")) {
+    menu.classList.remove("show");
+    menu_cover.classList.remove("show-cover");
+    bar.classList.remove("close");
   }
 }
-document.getElementById('container').addEventListener('click', hide_menu);
+document.getElementById("content").addEventListener("click", hide_menu);
